3869176bccb718eb3f49384dbf32c2a631b6876e
Maxim <maximyoutuber@yandex.ru>
2023-12-04 21:04:04 +0300

    First message

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Backend for MINDMINGLE
+## Стек, используемый для backend'a:
+### FastApi, Python 3.8, Postgersql
---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    os.system('uvicorn backend.main:app')
