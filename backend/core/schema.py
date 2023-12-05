fffa6753380bf11db7d125af4544cc500fb158c9
Maxim <maximyoutuber@yandex.ru>
2023-12-06 01:42:19 +0300

    Small fix

---
+++ backend/core/schema.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+from datetime import datetime
+class Default(BaseModel):
+    id:int
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
--- backend/startups/schema.py
+++ backend/startups/schema.py
@@ -1,13 +1,23 @@
+#
+#
+#       CREATED BY MAXIM SHESTAKOV: LYMOOS
+
 from pydantic import BaseModel
+from core.schema import Default
+from typing import List, Optional
 
 class StartUp_Create(BaseModel):
-    name:str
-    #Придумать схему и всё что должно быть
+    title:str
+    discription: str | None = None
+    owner_id:int
+    is_premium:bool
+    stack: List[str]
+    logo: bytes
+    photo_list: Optional[List[bytes]] = None
 
-class StartUp(StartUp_Create):
-    name:str
-    #Придумать схему и всё что должно быть
+class StartUp(StartUp_Create,Default):
+    status:bool
+    memberlist: Optional[List[int]] = None #id разработчиков, участвующих в стартапе
 
 class StartUp_Delete(BaseModel):
     id:int
-    #Придумать схему и всё что должно быть
