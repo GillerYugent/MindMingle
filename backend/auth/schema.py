53c0e18ded5236c609e0494bc8ea93fd3977be4d
Maxim <maximyoutuber@yandex.ru>
2023-12-06 02:06:08 +0300

    Small fix

--- backend/auth/schema.py
+++ backend/auth/schema.py
@@ -2,6 +2,19 @@ from pydantic import BaseModel, EmailStr
 from typing import List, Optional
 from backend.core.schema import Default
 
+#Класс для опыта
+class Experience(BaseModel):
+    role:str
+    company:str
+    description: Optional[str] = None
+
+#Класс для навыков
+class Skills(BaseModel):
+    name: str
+    level: str
+    description: Optional[str] = None
+
+#Классы для пользователей
 class UserCreate(BaseModel):
     username: str
     first_name:str
@@ -9,10 +22,10 @@ class UserCreate(BaseModel):
     email: EmailStr
     phone:str
     password: str
-    discription: str
+    description: str
     status: bool
-    skills = Optional[List[str]] = None
-    expereince = Optional[list[str]] = None
+    skills = Optional[List[Skills]] = None
+    experience = Optional[list[Experience]] = None
     is_active: bool = True
     is_superuser: bool = False
     is_verified: bool = False
@@ -22,3 +35,4 @@ class User(UserCreate,Default):
 
 class UserDelete(BaseModel):
     id:int
+
