cec6efebb7fd46bc3f3b88aba254049058f0e791
Maxim <maximyoutuber@yandex.ru>
2023-12-12 23:53:03 +0300

    Don't working. Wait fixes

--- backend/resume/endpoints/experience.py
+++ backend/resume/endpoints/experience.py
@@ -6,3 +6,19 @@ from fastapi import APIRouter
 from resume.schema import ExperiencCreate, Experience, ExperienceDelete
 
 router = APIRouter()
+
+@router.get('/{resume_id}',response_model=list[Experience])
+async def experience_get(resume_id:int) -> list[Experience]:
+    pass
+
+@router.get('/{experience_id}',response_class=Experience)
+async def experience_get_by_id(experience_id:int) -> Experience:
+    pass
+
+@router.post('/',response_class=Experience)
+async def experience_create(experience:ExperiencCreate) -> Experience:
+    pass
+
+@router.delete('/{experience_id}',response_class=ExperienceDelete)
+async def experience_delete(experience_id:int) -> ExperienceDelete:
+    pass
