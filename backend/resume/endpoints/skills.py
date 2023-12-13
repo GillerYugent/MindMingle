89123fb34c40e8bf8bab3dbe9d91a282a394f64f
Maxim <maximyoutuber@yandex.ru>
2023-12-14 00:26:10 +0300

    Creating schedule

--- backend/resume/endpoints/skills.py
+++ backend/resume/endpoints/skills.py
@@ -4,6 +4,21 @@
 
 from fastapi import APIRouter
 from resume.schema import Skill, SkillCreate, SkillDelete
-
+from uuid import UUID
 router = APIRouter()
 
+@router.get('/{resume_id}',response_model=list[Skill])
+async def skill_get(resume_id:UUID) -> list[Skill]:
+    pass
+
+@router.get('/{skill_id}',response_model=Skill)
+async def skill_get_by_id(skill_id:UUID) -> Skill:
+    pass
+
+@router.post('/',response_model=Skill)
+async def skill_create(skill:SkillCreate) -> Skill:
+    pass
+
+@router.delete('/{skill_id}',response_model=SkillDelete)
+async def skill_delete(skill_id:UUID) -> SkillDelete:
+    pass
--- backend/resume/routers.py
+++ backend/resume/routers.py
@@ -11,7 +11,7 @@ from resume.endpoints.skills import router as skills_router
 router = APIRouter()
 
 
-#router.include_router(skills_router)
+router.include_router(skills_router, prefix="/skill")
 router.include_router(education_router, prefix="/education")
 router.include_router(experience_router,prefix="/experience")
 router.include_router(resume_router)
